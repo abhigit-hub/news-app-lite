14b02a883fca41e99cb35d4e1cafe98b8a11c651
Abhijit Naik <naikabhiit@gmail.com>
2024-03-22 17:47:23 +0530

    Created a DI structure for the app
    Signed-off-by: Abhijit Naik <naikabhiit@gmail.com>

---
+++ app/src/main/java/com/compose/newsapplite/di/AppModule.kt
@@ -0,0 +1,22 @@
+package com.compose.newsapplite.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+            .build()
+    }
+}
