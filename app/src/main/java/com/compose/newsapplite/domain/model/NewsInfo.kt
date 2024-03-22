1d9d665fd83354f73415005fc326012ebf77ae38
Abhijit Naik <naikabhiit@gmail.com>
2024-03-23 01:50:01 +0530

    Added workaround logic to handle random null objects returned from the server
    Signed-off-by: Abhijit Naik <naikabhiit@gmail.com>

--- app/src/main/java/com/compose/newsapplite/domain/model/NewsInfo.kt
+++ app/src/main/java/com/compose/newsapplite/domain/model/NewsInfo.kt
@@ -1,15 +1,12 @@
 package com.compose.newsapplite.domain.model
 
 data class NewsInfo(
-    val status: String,
-    val totalResults: Int,
     val articles: List<ArticleInfo>
 )
 
 data class ArticleInfo(
     val author: String,
     val content: String,
-    val description: String,
     val publishedAt: String,
     val source: SourceInfo,
     val title: String,
@@ -18,6 +15,5 @@ data class ArticleInfo(
 )
 
 data class SourceInfo(
-    val id: String,
     val name: String
 )
